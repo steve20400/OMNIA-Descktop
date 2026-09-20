5381bfbd8132837d635f8dd487f4acb7425d6f7c
STEVE MANFO <stevelove2040@gmail.com>
2026-09-20 03:42:09 +0000

    fix(mini-player): importer flutter/services.dart pour LogicalKeyboardKey et KeyDownEvent

--- lib/ui/widgets/mini_player.dart
+++ lib/ui/widgets/mini_player.dart
@@ -2,6 +2,7 @@ import 'dart:math' as math;
 
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:window_manager/window_manager.dart';
 
